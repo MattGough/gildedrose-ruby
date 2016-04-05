ecdc0ac73b7f21cb5df83c8df760c32abe578a22
Matt <mattpgough@gmail.com>
2016-04-05 16:10:02 +0100

    Added test for to_s method returning a combined string of item attributes.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,13 +1,25 @@
 require './item'
 
 describe Item do
-  subject(:item) { described_class.new("item1", 20, 10) }
+  subject(:item) { described_class.new("Item1", 20, 10) }
 
   it 'has a name attribute' do
-    expect(item.name).to eq "item1"
+    expect(item.name).to eq "Item1"
+  end
+
+  it 'has a sell_in attribute' do
+    expect(item.sell_in).to eq 20
+  end
+
+  it 'has a quality attribute' do
+    expect(item.quality).to eq 10
   end
 
   describe '#to_s' do
     it { is_expected.to respond_to :to_s }
+
+    it 'returns a string of the attributes combined' do
+      expect(item.to_s).to eq("Item1, 20, 10")
+    end
   end
 end
