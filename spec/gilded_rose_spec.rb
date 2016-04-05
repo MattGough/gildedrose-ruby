a7eeb1b464d8e7757bf6e6700d2c4f1a337e2cbb
Matt <mattpgough@gmail.com>
2016-04-05 17:07:16 +0100

    Constructed tests for update_quality method, reducing quality and sell_in by one.

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,10 +1,18 @@
 require './gilded_rose'
 
 describe GildedRose do
-  subject(:gildedrose) { described_class.new(item) }
-  let(:item) { double :item }
-
   describe '#update_quality' do
-    it { is_expected.to respond_to :update_quality }
+
+    it 'should reduce the quality by 1' do
+      items = [Item.new("item1", 20, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 9
+    end
+
+    it 'should reduce the sell_in by 1' do
+      items = [Item.new("item1", 10, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq 9
+    end
   end
 end
